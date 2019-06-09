0164ded12a31878c0219280c75b5f77048e59145
Tamar Auber <mischegoss@gmail.com>
2019-06-09 17:53:02 -0400

    Updates

---
+++ precache-manifest.0c26902aa73753adfb098d6308ec879d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ab7ec3dd27a90d18f6921a90b7a6a6",
+    "url": "/mischegoss/Google-Books/index.html"
+  },
+  {
+    "revision": "96f19f2666b2d3d05f18",
+    "url": "/mischegoss/Google-Books/static/css/main.7e54f9a1.chunk.css"
+  },
+  {
+    "revision": "18911cd32079849d3270",
+    "url": "/mischegoss/Google-Books/static/js/2.13e6048a.chunk.js"
+  },
+  {
+    "revision": "96f19f2666b2d3d05f18",
+    "url": "/mischegoss/Google-Books/static/js/main.e4983f78.chunk.js"
+  },
+  {
+    "revision": "935d123bcdfa0e22288a",
+    "url": "/mischegoss/Google-Books/static/js/runtime~main.3c421d54.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/mischegoss/Google-Books/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/mischegoss/Google-Books/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/mischegoss/Google-Books/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
